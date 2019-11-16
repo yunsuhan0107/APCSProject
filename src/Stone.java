2ccb67e6d50629b7acccdf3bdd1fe047fb6dd235
Yunsu Han <yunsuhan00107@gmail.com>
2019-11-16 16:38:17 +0900

    Lab 9

---
+++ src/Stone.java
@@ -0,0 +1,22 @@
+// APCS Lab 9 Critters
+// Stone objects are displayed as S and always stay put.
+// They always pick ROAR in a fight.
+
+import java.awt.*;
+
+public class Stone extends Critter {
+	// method comment goes here
+	public Attack fight(String opponent) {
+		return Attack.ROAR;
+	}
+
+	// method comment goes here
+	public Color getColor() {
+		return Color.GRAY;
+	}
+
+	// method comment goes here
+	public String toString() {
+		return "S";
+	}
+}
